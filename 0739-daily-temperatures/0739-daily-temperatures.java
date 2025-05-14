36ac5b068b5b30e33a4ed24f159a61448b0d083b
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-05-14 20:44:00 +0900

    Time: 62 ms (67.84%), Space: 63.2 MB (13.88%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,39 +1,31 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         Stack<Integer> mono = new Stack<>();
-        int[] ngm = new int[temperatures.length]; //ngm stands for next greatest number
-
+        int length = temperatures.length;
+        int[] ans = new int[length];
         mono.push(0);
-        for(int i = 1; i < temperatures.length; i++){
-            int index = i;
-            while(!mono.isEmpty() && temperatures[i] > temperatures[mono.peek()]){ //접근 중인 원소의 값이 스택의 탑보다 크면 크기가 맞을 때 까지 다 끄집어내서 넣고 스택에는 자신의 인덱스.
-                index = mono.pop();
-                ngm[index] = i - index;
+
+        for (int i = 1; i < length; i++) {
+            //대소 비교해서 다 끄집어 낸다음에 인덱스 정보 구함
+            int idx = i;
+            while (!mono.isEmpty() && temperatures[mono.peek()] < temperatures[i]) {//73 74 75 입력받고 71입력받으면
+            //
+                idx = mono.pop();
+                ans[idx] = i - idx;
             }
+
+            //인덱스 정보 넣기. 끄집어 내진놈
             mono.push(i);
         }
-           return ngm;
-    }
 
-
-}
-
-/*
-class Solution {
-    public int[] dailyTemperatures(int[] temperatures) {
-        int[] answer = new int[temperatures.length];
-        for(int i = 0; i < temperatures.length; i++){
-            int count = 0;
-            int max = temperatures[i];
-            for(int j = i + 1; j < temperatures.length; j++){
-                if(temperatures[j] > max){
-                    answer[i] = ++count;
-                    break;
-                }
-                else count++;
-            }
-        }
-        return answer;
+        return ans;
     }
 }
-*/
+/**
+mono: 73
+current 74
+current 75
+current 
+
+question is mono.peek() > current OR mono.peek() < current
+ */
