f4395143135270ab852e50b230c870b1e6baa86d
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-07-25 20:15:42 +0900

    Time: 7 ms (90.41%), Space: 44.7 MB (82.72%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -4,21 +4,20 @@ class Solution {
         int l = 0;
         //int r = 0;
 
-        Map<Character, Integer> map = new HashMap<>();
-        
+        //Map<Character, Integer> map = new HashMap<>();
+        int[] freq = new int[26];
         int maxf = 0;
         for (int r = 0; r < s.length(); r++) {
-            map.put(s.charAt(r),
-            map.getOrDefault(s.charAt(r), 0) + 1
-            );
+            
+            freq[s.charAt(r) - 'A'] += 1;
 
             // for (int f : map.values()) {
             //     maxf = Math.max(f, maxf);
             // }
-            maxf = Math.max(maxf, map.get(s.charAt(r)));
+            maxf = Math.max(maxf, freq[s.charAt(r) - 'A']);
 
             while (r - l + 1 - maxf > k) {
-                map.put(s.charAt(l), map.get(s.charAt(l)) - 1);
+                freq[s.charAt(l) - 'A'] -= 1;
                 l++;
             }
             res = Math.max(res, r - l + 1);
