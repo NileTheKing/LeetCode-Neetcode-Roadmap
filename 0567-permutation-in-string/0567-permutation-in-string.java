6712367b1372fa56c315a548c5a2204618a1fc10
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-07-26 16:16:30 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,40 +1,30 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
         int length = s1.length();
-        int r = length;
-        int l = 0;
-        int[] freq1 = new int[26]; //s1 빈도수
-        int[] freq2 = new int[26]; //s2 slide 빈도수
-
-        //s1빈도수 저장
-        for (char c : s1.toCharArray()){
-            freq1[c - 'a'] += 1;
-        }
-
-        //s2 sliding window 초기값
+        
         for (int i = 0; i < length; i++) {
-            freq2[s2.charAt(i) - 'a'] += 1;
+            freq1[s1.charAt(i) - 'a']++;
+            freq2[s2.charAt(i) - 'a']++;
         }
+
+        for (int i = length - 1; i < s2.length() -1; i++) {
+            if (Arrays.equals(freq1, freq2)) return true;
+
+            int l = i - length + 1;
+            freq2[s2.charAt(i+1) - 'a']++;
+            freq2[s2.charAt(l) - 'a']--;
+        }
+
+        if (Arrays.equals(freq1, freq2)) return true;
+        else return false;
+
         
-        while (r < s2.length()) { //순회하면서 슬라이딩 윈도우를 조작해서 일치하는지 확
-
-            if (Arrays.equals(freq1, freq2)) {
-                return true;
-            }
-
-            freq2[s2.charAt(r) - 'a'] += 1;
-            freq2[s2.charAt(l) - 'a'] -= 1;
-            l++;
-            r++;
-            
-            //r기준으로 fixed sliding window의 빈도수가 일치하는지 확인
-            
-        }
-        
-        if (Arrays.equals(freq1, freq2)) {
-                return true;
-        }
-
-        return false;
     }
 }
+/**
+s1의 정보를 array든 hash든 저장을 한다
+s2를 순회하며 two ptrs를 사용한다.
+r은 계속 순회하며 l과 r 사이의 substring의 정보(빈도수)와 s1의 정보가 동일하다면 참
+ */
