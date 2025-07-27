e155166657891b33bb4ddc94a2102a8e4ffa54c9
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-07-27 22:03:57 +0900

    Time: 4 ms (88.07%), Space: 58.3 MB (58.78%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -9,22 +9,14 @@ class Solution {
 
             if (slow == fast) break;
         }
-
         int slow2 = 0;
+
         while (true) {
             slow = nums[slow];
             slow2 = nums[slow2];
 
-
             if (slow == slow2) return slow;
         }
+        
     }
 }
-/**
-constant extra space -> no hashmap
-without modifying -> no sorting
-
-1-n이면 n개의 숫자가 있어야함(오름차순)
-n+1개이려면 2개가 중복이어야함.
-만약 3개가 중복에다가 n+1개라면 중복3개이니까 하나가 비어야함.
- */
