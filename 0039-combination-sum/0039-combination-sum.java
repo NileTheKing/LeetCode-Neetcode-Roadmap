194a8901c1c2be2d7b1f2d4b750ef2e410992c94
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-07-31 11:34:59 +0900

    Time: 3 ms (21.57%), Space: 44.3 MB (97.21%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
+        dfs(candidates, new ArrayList<>(), 0, target, 0);
+
+        return ans;
+    }
+    public void dfs(int[] candidates, List<Integer> current, int currentSum, int target, int idx) {
+        if (currentSum == target) ans.add(new ArrayList<>(current));
+        if (currentSum > target) return;
+
+        for (int i = idx; i < candidates.length; i++) {
+            current.add(candidates[i]);
+            dfs(candidates, current, currentSum + candidates[i], target, i);
+            current.remove(current.size() - 1);
+        }
+    }
+}
