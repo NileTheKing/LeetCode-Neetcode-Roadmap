83479d9ad90c51bc16dd55b0d4c5ba258c1b8348
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-01 11:09:38 +0900

    Time: 1 ms (92.77%), Space: 44.2 MB (98.69%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,22 +1,27 @@
 class Solution {
-    List<List<Integer>> res = new ArrayList<>();
+    List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> permute(int[] nums) {
-           
-            dfs(nums, new ArrayList<>(), new boolean[nums.length], 0);
-           return res;
+        
+        backtrack(nums, new boolean[nums.length], new ArrayList<>());
+
+        return ans;
     }
-    public void dfs(int[] nums, List<Integer> current, boolean[] visited, int depth) {
-        if (depth == nums.length) {
-            res.add(new ArrayList<>(current));
+    public void backtrack(int[] nums, boolean[] visited, List<Integer> current) {
+
+        if (current.size() == nums.length) {
+            ans.add(new ArrayList<>(current));
             return;
         }
+
         for (int i = 0; i < nums.length; i++) {
-            if (visited[i]) continue;
-            visited[i] = true;
-            current.add(nums[i]);
-            dfs(nums, current, visited, depth + 1);
-            current.remove(current.size() - 1);
-            visited[i] = false;
+            if (!visited[i]) {
+                visited[i] = true;
+                current.add(nums[i]);
+                backtrack(nums, visited, current);
+                current.remove(current.size() - 1);
+                visited[i] = false;
+            }
         }
     }
+
 }
