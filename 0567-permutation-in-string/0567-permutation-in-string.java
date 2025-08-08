ca01ef74b48e285c1ad2061bedb162e38135a123
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-08 09:45:45 +0900

    Time: 9 ms (48.68%), Space: 42.8 MB (57.41%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,44 +1,38 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-
+ 
         int length1 = s1.length();
         int length2 = s2.length();
 
         if (length1 > length2) return false;
-        int r = length1;
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
         int l = 0;
-        int[] freq1 = new int[26]; //s1 빈도수
-        int[] freq2 = new int[26]; //s2 slide 빈도수
-
-        //s1빈도수 저장
-        for (char c : s1.toCharArray()){
-            freq1[c - 'a'] += 1;
-        }
-
-        //s2 sliding window 초기값
+        int r = length1;
+        
         for (int i = 0; i < length1; i++) {
-            freq2[s2.charAt(i) - 'a'] += 1;
+            freq1[s1.charAt(i) - 'a']++;
+            freq2[s2.charAt(i) - 'a']++;
         }
-        
-        while (r < s2.length()) { //순회하면서 슬라이딩 윈도우를 조작해서 일치하는지 확
 
-            if (Arrays.equals(freq1, freq2)) {
-                return true;
-            }
+        while (r < length2) {
+            if (Arrays.equals(freq1, freq2)) return true;
 
-            freq2[s2.charAt(r) - 'a'] += 1;
-            freq2[s2.charAt(l) - 'a'] -= 1;
-            l++;
+            freq2[s2.charAt(r) - 'a']++;
+            freq2[s2.charAt(l) - 'a']--;
             r++;
-            
-            //r기준으로 fixed sliding window의 빈도수가 일치하는지 확인
-            
-        }
-        
-        if (Arrays.equals(freq1, freq2)) {
-                return true;
+            l++;
         }
 
-        return false;
+        if (Arrays.equals(freq1, freq2)) return true;
+        else return false;
+
+        
     }
 }
+/**
+s1의 정보를 array든 hash든 저장을 한다
+s2를 순회하며 two ptrs를 사용한다.
+r은 계속 순회하며 l과 r 사이의 substring의 정보(빈도수)와 s1의 정보가 동일하다면 참
+ */
