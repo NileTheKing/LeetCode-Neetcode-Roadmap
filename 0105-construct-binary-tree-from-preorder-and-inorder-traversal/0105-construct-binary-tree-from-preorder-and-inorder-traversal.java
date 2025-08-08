f2bc098ba6492c25ed04f8003e171b0374e8ceb2
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-08 14:24:15 +0900

    Time: 1 ms (96.05%), Space: 44.6 MB (32.3%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -13,25 +13,28 @@
  *     }
  * }
  */
-class Solution{
+class Solution {
     Map<Integer, Integer> indices = new HashMap<>();
     int preIdx = 0;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-
         for (int i = 0; i < inorder.length; i++) {
             indices.put(inorder[i], i);
-        } 
-
-        return dfs(preorder, 0, inorder.length - 1);
-
+        }
+        return construct(preorder, inorder, 0, inorder.length - 1);
     }
-    private TreeNode dfs(int[] preorder, int l, int r) {
-        if (l > r) return null;
+    public TreeNode construct(int[] preorder, int[] inorder, int l, int r) {
+        if(l > r) return null;
         int root_val = preorder[preIdx++];
-        TreeNode root = new TreeNode(root_val);
-        int mid = indices.get(root_val);
-        root.left = dfs(preorder, l, mid - 1);
-        root.right = dfs(preorder, mid + 1, r);
-        return root;
+        int rootIdx = indices.get(root_val); //이제 inorder에서의 루트의 인덱스를 앎
+        TreeNode node = new TreeNode(root_val);
+        node.left = construct(preorder, inorder, l, rootIdx - 1);
+        node.right = construct(preorder, inorder, rootIdx + 1, r);
+
+        return node;
     }
 }
+/**
+트리는 재귀적인 특성을 가질 수 있음을 인지
+preordrer: 자기 먼저 방문. 일단 루트 알 수 있음.
+inorder: 얘는 왼쪽 서브트리와 오른쪽 서브트리의 사이즈를 알 수 있음 -> 이것이 무엇을 의미하나?
+ */
