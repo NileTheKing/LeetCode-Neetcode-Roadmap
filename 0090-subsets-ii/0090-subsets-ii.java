7e6c0d1fcf4bad100fbe6cbf65b373526c483b63
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-11 13:41:07 +0900

    Time: 1 ms (99.95%), Space: 43.6 MB (61.55%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -4,12 +4,13 @@ class Solution {
         Arrays.sort(nums);
         dfs(nums, new ArrayList<>(), 0);
 
-        return res.stream().distinct().collect(Collectors.toList());
+        return res;
     }
     public void dfs(int[] nums, List<Integer> current, int idx) {
         
         res.add(new ArrayList<>(current));
         for (int i = idx; i < nums.length; i++) {
+            if (i > idx && nums[i] == nums[i - 1]) continue;
             current.add(nums[i]);
             dfs(nums, current, i + 1);
             current.remove(current.size() - 1);
