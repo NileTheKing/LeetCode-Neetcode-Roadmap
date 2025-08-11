2aefd4b19164bfac374e37d7930fcaa1c5494dd6
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-11 10:08:22 +0900

    Time: 2 ms (99.81%), Space: 42.9 MB (97.99%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,33 +1,28 @@
 class Solution {
-
     List<List<Integer>> ans = new ArrayList<>();
+    //int length;
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        
-        dfs(candidates, 0, target , 0, new ArrayList<>());
+        //length = candidates.length;
+        backtrack(candidates, target, 0, 0, new ArrayList<>());
 
-        // //중복제거
-        // Set<List<Integer>> set = new HashSet<>();
-        // for(List<Integer> list : ans) {
-        //     set.add(list);
-        // }
         return ans;
     }
-    public void dfs(int[] candidates, int total, int target, int idx, List<Integer> current) {
+    public void backtrack(int[] candidates, int target, int total, int idx, List<Integer> current) {
+ 
         if (total == target) {
-            ans.add(new ArrayList<>(current));
+            ans.add(new ArrayList<>(current)) ;
             return;
         }
+        //if (idx == length) return;
 
         for (int i = idx; i < candidates.length; i++) {
-            if (candidates[i] + total > target) {
-                return;
-            }
-            if (i > idx && candidates[i] == candidates[i - 1]) continue;//다음거 확인
-           
+            if (i > idx && candidates[i] == candidates[i -1]) continue;
+            if (total + candidates[i] > target) return; //정렬 되어있으니까 candiate이 너무 크다면 뒤에 for문은 볼필요도 없다
             current.add(candidates[i]);
-            dfs(candidates, total + candidates[i], target, i + 1, current);
+            backtrack(candidates, target, total + candidates[i], i + 1, current);
             current.remove(current.size() - 1);
         }
+        return;
     }
 }
