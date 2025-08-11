9b008f4c134d5d59bc5086c1649def9c3efd0dc9
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-11 11:06:40 +0900

    Time: 211 ms (18.73%), Space: 45.1 MB (18.77%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,38 +1,39 @@
 class Solution {
-    int[][] directions = {{1,0},{-1,0},{0,1}, {0,-1}};
+    int[][] directions = {{-1,0},{1,0},{0,1},{0,-1}};
+    int m,n;
     public boolean exist(char[][] board, String word) {
-
-        //boolean[][] visited = new boolean[board.length][board[0].length];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    boolean[][] visited = new boolean[board.length][board[0].length];
-                    visited[i][j] = true;
-                    if (dfs(board, word, 1, visited, i, j)) {
-                        //System.out.println("got true");
-                        return true;
-                    }
-                    visited[i][j] = false;
-                }
+        m = board.length;
+        n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, new int[]{i,j}, new boolean[m][n], word, 0)) return true;
             }
         }
         return false;
     }
-    public boolean dfs(char[][] board, String word, int idx, boolean[][] visited, int r, int c) {
-        if (idx == word.length()) { //System.out.println("returning true"); 
-        return true;}
-
-        for (int[] d : directions) {
-            int nr = r + d[0];
-            int nc = c + d[1];
-            if (nr >= board.length || nr < 0 || nc >= board[0].length || nc < 0 || visited[nr][nc]) continue;
-            if (board[nr][nc] != word.charAt(idx)) continue;
-            //System.out.println("visiting [" + nr + ", " + nc);
-            visited[nr][nc] = true;
-            if (dfs(board, word, idx + 1, visited, nr, nc)) return true;
-            visited[nr][nc] = false;
-        }
+    public boolean dfs(char[][] board, int[] coord, boolean[][] visited, String word, int idx) {
         
+        if (word.charAt(idx) != board[coord[0]][coord[1]]) return false;
+        if (idx == word.length()- 1) return true; //같고 마지막에 도달했는지
+
+        visited[coord[0]][coord[1]] = true;
+        for (int[] direction : directions) {
+            int nr = coord[0] + direction[0];
+            int nc = coord[1] + direction[1];
+
+            if (nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc]) {
+                if (dfs(board, new int[]{nr,nc}, visited, word, idx + 1)) {
+                    return true; // 만약 재귀 호출에서 단어를 찾았으면, 즉시 true를 반환하여 상위 호출로 전달
+                }
+            }
+        }
+        //여기까지 왔다면 답없는거임
+        visited[coord[0]][coord[1]] = false;
         return false;
     }
 }
+/**
+board를 순회하며 word에 맞는 지점을 따라 이동
+
+
+ */
