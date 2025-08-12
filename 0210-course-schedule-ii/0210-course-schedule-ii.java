45bc0fa0d4c5b22e8bca0e7ec70a03d85b33ab53
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-12 13:06:44 +0900

    Time: 53 ms (5.03%), Space: 46.3 MB (31.16%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,39 +1,51 @@
 class Solution {
-
+    List<Integer> ans;
+    Set<Integer> visited;
+    Map<Integer, List<Integer>> map;
+    int n;
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        n = numCourses;
+        map = new HashMap<>();
+        ans = new ArrayList<>();
+        visited = new HashSet<>();
         
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        Set<Integer> visited = new HashSet<>();
-        List<Integer> res = new ArrayList<>();
         for (int[] p : prerequisites) {
             map.computeIfAbsent(p[0], k -> new ArrayList<>()).add(p[1]);
         }
-
-        for (int i = 0; i < numCourses; i++) {
-            if(!dfs(i, map, visited, res, new HashSet<>())) return new int[0];
+        
+        for (int i = 0; i < n; i++) {
+            if (!isValid(new HashSet<>(), i, visited))
+                return new int[0];
         }
-        return res.stream().mapToInt(i->i).toArray();
-    }
-    public boolean dfs(int current, Map<Integer, List<Integer>> map,  Set<Integer> visited, List<Integer> res, Set<Integer> cycle) {
-        if (visited.contains(current)) return true;
-        if (cycle.contains(current)) return false;
-
-
-        cycle.add(current);
-
+        return ans.stream().mapToInt(a->a).toArray();
     
-            for (int nei : map.getOrDefault(current, new ArrayList<>())) {
-                if (!dfs(nei, map, visited,res, cycle)) return false;
-            }
+    }
+    public boolean isValid(Set<Integer> visiting ,int current, Set<Integer> visited) {
+        //System.out.println("currnet:" + current);
+        if (visited.contains(current)) {
+            //System.out.println("size condition ");
+            System.out.println("already visited and assured");
+            return true;
+        }
+        if (visiting.contains(current)) { 
+            //System.out.println("cycle detected"); 
+            return false;}
+         //cycle detected
+        
+        visiting.add(current);
 
+
+        for (var nei : map.getOrDefault(current, new ArrayList<>())) {
+            System.out.println("nei:" +  nei);
+            if (!isValid(visiting, nei,visited)) return false;
+        }
+        
+        System.out.println(current + " added to visited");
         visited.add(current);
-        cycle.remove(current);
-        res.add(current);
+        visiting.remove(current);
+        //System.out.println("reached end. coudlnt find. current: " + current);
+        ans.add(current);
         return true;
+
     }
 }
-/**
-순회를 해서 방문 경로를 추적. 다 방문하면 그걸 리턴
-불가능하면(싸이클 생기면) empty array 리턴
-    불가능한건
- */
