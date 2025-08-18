1549aac5cd486c683569db87aa38c880a604fe95
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-18 15:24:01 +0900

    Time: 0 ms (100%), Space: 44.4 MB (24.84%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,24 +15,14 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        
         return dfs(root, null, null);
     }
-    public boolean dfs(TreeNode node, Long min, Long max) {
-
+    public boolean dfs(TreeNode node, Integer min, Integer max) {
         if (node == null) return true;
 
-        //다음으로 재귀할 떄 오른쪽으로 간다면 최대값을 넘겨주고
-        //왼쪽으로 간다면 ㅗ치솟값을 넘겨줌.
-        if (max != null) 
-            if (node.val >= max) return false;
-        if (min != null)
-            if (node.val <= min) return false;
+        if (max != null && node.val <= max) return false;
+        if (min != null && node.val >= min) return false;
 
-        // dfs(node.left, min, null);
-        // dfs(node.right, null, max);
-
-        // return true; 
-        return dfs(node.left, min,  (long)node.val) && dfs(node.right, (long)node.val, max);
+        return dfs(node.left, node.val, max) && dfs(node.right, min, node.val);
     }
 }
