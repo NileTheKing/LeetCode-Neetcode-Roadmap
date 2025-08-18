37e39c6a47bfcc4f9dd93e11d25d09bd11b962e8
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-18 15:53:00 +0900

    Time: 10 ms (43.26%), Space: 42.9 MB (57.39%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,38 +1,23 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
- 
-        int length1 = s1.length();
-        int length2 = s2.length();
-
-        if (length1 > length2) return false;
-
+        if (s1.length() > s2.length()) return false;
         int[] freq1 = new int[26];
         int[] freq2 = new int[26];
-        int l = 0;
-        int r = length1;
-        
-        for (int i = 0; i < length1; i++) {
+
+        for (int i = 0; i < s1.length(); i++) {
             freq1[s1.charAt(i) - 'a']++;
             freq2[s2.charAt(i) - 'a']++;
         }
-
-        while (r < length2) {
-            if (Arrays.equals(freq1, freq2)) return true;
-
-            freq2[s2.charAt(r) - 'a']++;
-            freq2[s2.charAt(l) - 'a']--;
-            r++;
-            l++;
-        }
-
         if (Arrays.equals(freq1, freq2)) return true;
-        else return false;
 
-        
+        for (int i = 1; i < s2.length() - s1.length()+1; i++) { //4 - 1 - 3 -> 1
+            freq2[s2.charAt(i-1) - 'a']--;
+            freq2[s2.charAt(i + s1.length()-1) - 'a']++;
+            if (Arrays.equals(freq1, freq2)) return true;
+        }
+        return false;
     }
 }
 /**
-s1의 정보를 array든 hash든 저장을 한다
-s2를 순회하며 two ptrs를 사용한다.
-r은 계속 순회하며 l과 r 사이의 substring의 정보(빈도수)와 s1의 정보가 동일하다면 참
+substring
  */
