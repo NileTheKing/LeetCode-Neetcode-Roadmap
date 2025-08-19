989f01a363d549227f1bd66b5082027ffbeaf34a
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-19 10:48:28 +0900

    Time: 2 ms (99.84%), Space: 43.9 MB (5.55%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,28 +1,32 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
-    //int length;
+    List<List<Integer>> ans;
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        //length = candidates.length;
-        backtrack(candidates, target, 0, 0, new ArrayList<>());
+        ans = new ArrayList<>();
 
+        dfs(candidates, target, 0, new ArrayList<>(), 0);
         return ans;
     }
-    public void backtrack(int[] candidates, int target, int total, int idx, List<Integer> current) {
- 
-        if (total == target) {
-            ans.add(new ArrayList<>(current)) ;
+    public void dfs(int[] candidates, int target, int idx, List<Integer> current, int cSum) {
+
+        if (cSum == target) {
+            ans.add(new ArrayList<>(current));
             return;
         }
-        //if (idx == length) return;
+        if (idx == candidates.length) return;
 
         for (int i = idx; i < candidates.length; i++) {
-            if (i > idx && candidates[i] == candidates[i -1]) continue;
-            if (total + candidates[i] > target) return; //정렬 되어있으니까 candiate이 너무 크다면 뒤에 for문은 볼필요도 없다
+            if (i > idx && candidates[i] == candidates[i-1]) continue;
+            if (cSum + candidates[i] > target) return;
+
             current.add(candidates[i]);
-            backtrack(candidates, target, total + candidates[i], i + 1, current);
-            current.remove(current.size() - 1);
+            dfs(candidates, target, i + 1, current, cSum + candidates[i]);
+            current.remove(current.size() -1);
         }
         return;
     }
 }
+/**
+1 1 2 5 6 7 10
+
+ */
