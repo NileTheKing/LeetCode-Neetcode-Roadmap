648f3aeb9a5a452c25e2dd45057cfcbba58e0953
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-19 09:11:47 +0900

    Time: 6 ms (28.97%), Space: 45.2 MB (38.14%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,13 +10,21 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
         if (Math.max(p.val, q.val) < root.val) {
             return lowestCommonAncestor(root.left, p, q);
         }else if (Math.min(p.val, q.val) > root.val) {
             return lowestCommonAncestor(root.right, p, q);
-        }else { //같은 경우 포함됨. 결국 찾다보면 어느 순간 이런 지점이 오는데 이게 답
+        }else { // 왼쪽에 하나 오른쪽에 하나 or 자기자신이랑 자식..그니까 위의 케이스가 아닌 경우.
+            //답
             return root;
         }
+    
     }
 }
+
+/**
+순회해서 찾아야함. 그런데 bst임. -> 탐색 범위 지정
+    둘중 더 작은게 만약 root보다 크면 오른쪽 가야함
+    둘중 더 큰 게 더 작으면 왼쪽으로 가야함.
+    그게 아니라면 양쪽으로 
+ */
