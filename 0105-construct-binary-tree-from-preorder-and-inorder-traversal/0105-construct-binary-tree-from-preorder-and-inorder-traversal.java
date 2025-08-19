9e333f29f9ead4b93fa2c07850e4e620f60f0479
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-19 10:31:50 +0900

    Time: 1 ms (96.23%), Space: 44.4 MB (51.99%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,27 +14,41 @@
  * }
  */
 class Solution {
-    Map<Integer, Integer> indices = new HashMap<>();
     int preIdx = 0;
+    Map <Integer, Integer> map;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        map = new HashMap<>();
         for (int i = 0; i < inorder.length; i++) {
-            indices.put(inorder[i], i);
+            map.put(inorder[i], i);
         }
-        return construct(preorder, inorder, 0, inorder.length - 1);
+        return dfs(preorder, inorder, 0, inorder.length -1);   
     }
-    public TreeNode construct(int[] preorder, int[] inorder, int l, int r) {
-        if(l > r) return null;
-        int root_val = preorder[preIdx++];
-        int rootIdx = indices.get(root_val); //이제 inorder에서의 루트의 인덱스를 앎
-        TreeNode node = new TreeNode(root_val);
-        node.left = construct(preorder, inorder, l, rootIdx - 1);
-        node.right = construct(preorder, inorder, rootIdx + 1, r);
+    public TreeNode dfs(int[] preorder, int[] inorder, int l, int r) {
+        
+        if (l > r) return null;
+        int rootVal = preorder[preIdx++];
+        int rootIdx = map.get(rootVal);
+
+        TreeNode node = new TreeNode(rootVal);
+        node.left = dfs(preorder, inorder, l, rootIdx-1);
+        node.right = dfs(preorder, inorder, rootIdx+1, r);
 
         return node;
     }
+
 }
 /**
-트리는 재귀적인 특성을 가질 수 있음을 인지
-preordrer: 자기 먼저 방문. 일단 루트 알 수 있음.
-inorder: 얘는 왼쪽 서브트리와 오른쪽 서브트리의 사이즈를 알 수 있음 -> 이것이 무엇을 의미하나?
+recursively construct the tree.     
+how?
+    => what preoreder tells is is that the first node of the array is the root of the tree.
+    and what inorder array tells us is the left subtree of the root and also the right.
+    so if you can recursively iterate those arrays and traverse the tree, you can build a complete tree
+    say you start with 3 becuz first node of preorder is 3. then according to inorder array, you know the left part is 9, the right part is 3,15,20,7
+    then you look for the elements that come before the root of the inorder array, which is 9. then you recursively construct the subtree for the root 9.
+    the left subtree is empty, right subtree of 9 is empty becuz there is nothing between 3 and 9 according to inrorder array.
+    Back to the 3, original root, its right subtree would be 3,15,20,7. and its root is 20 according to preorder.
+    the left part of 20 would be 15 and for the right it would be 7. that's pretty much it. 
+
+    So get into the code. we need a dfs function that comes with left parameter, right parameter for the inorder array to construct the subtrees. 
+
  */
