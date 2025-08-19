c5823ca87e6375c0878c06e4ab293f514efb25ee
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-19 09:04:49 +0900

    Time: 4 ms (88.95%), Space: 58.2 MB (58.19%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,5 +1,6 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        
         int slow = 0;
         int fast = 0;
 
@@ -9,14 +10,17 @@ class Solution {
 
             if (slow == fast) break;
         }
-        int slow2 = 0;
 
+        int slow2 = 0;
         while (true) {
             slow = nums[slow];
             slow2 = nums[slow2];
 
             if (slow == slow2) return slow;
         }
-        
     }
 }
+/**
+find the cycle
+slow fast
+ */
