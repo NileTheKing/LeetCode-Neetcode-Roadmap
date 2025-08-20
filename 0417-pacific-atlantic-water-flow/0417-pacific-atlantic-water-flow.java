fd804fddb4ccd11ef6e274ddb4d682c8655b00f9
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-20 13:39:59 +0900

    Time: 5 ms (55.88%), Space: 45.7 MB (47.39%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,28 +1,21 @@
 class Solution {
-    int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
     int m, n;
+    int[][] directions = {{-1,0},{1,0},{0,1},{0,-1}};
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         m = heights.length;
         n = heights[0].length;
-
-        //pacific to uphill
         boolean[][] pacific = new boolean[m][n];
         boolean[][] atlantic = new boolean[m][n];
-        for (int i = 0; i < m; i++) { //|
-            bfs(heights, pacific, i, 0);
-        }
-        for (int i = 0; i < n; i++) { // -
-            bfs(heights, pacific, 0, i);
-        }
-        //atlantic to uphill
-         for (int i = 0; i < m; i++) { // |
-            bfs(heights, atlantic, i, n-1);
-        }
-        for (int i = 0; i < n; i++) {// -
-            bfs(heights, atlantic, m-1, i);
-        }
-        //intersection
         List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            dfs(heights, pacific, i, 0);
+            dfs(heights, atlantic, i, n-1);
+        }
+        for (int i = 0; i < n; i++) {
+            dfs(heights, pacific, 0, i);
+            dfs(heights, atlantic, m-1, i);
+        }
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (pacific[i][j] && atlantic[i][j]) {
@@ -30,28 +23,27 @@ class Solution {
                 }
             }
         }
+
         return ans;
+
+
     }
-    public void bfs(int[][] heights, boolean[][] reachable, int r, int c) {
-        Queue<int[]> q = new LinkedList<>();
-        //boolean[][] visited = new boolean[m][n];
-        q.offer(new int[] {r,c});
-        //visited[r][c] = true;
-        reachable[r][c] = true;
+    public void dfs(int[][] heights, boolean[][] ocean, int r, int c) {
+        if (ocean[r][c]) return;
 
-        while (!q.isEmpty()) {
-            int[] polled = q.poll();
-            for (var d : directions) {
-                int nr = polled[0] + d[0];
-                int nc = polled[1] + d[1];
+        ocean[r][c] = true;
 
-                if (nr < 0 || nr >= m || nc < 0 || nc >= n || reachable[nr][nc]) continue;
-                if (heights[polled[0]][polled[1]] <= heights[nr][nc]) {
-                    q.offer(new int[] {nr,nc});
-                    reachable[nr][nc] = true;
-                    //visited[nr][nc] = true;
-                }
-            }
+        for (int[] d : directions) {
+            int nr = r + d[0];
+            int nc = c + d[1];
+
+            if (nr < 0 || nr >= m || nc < 0 || nc >=n  || heights[r][c] > heights[nr][nc]) continue;
+            dfs(heights, ocean, nr, nc);
         }
+        return;
+        
     }
 }
+/**
+
+ */
