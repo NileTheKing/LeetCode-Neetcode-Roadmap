1927f64079c195a53d76f90877ccca8e5a138877
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-21 11:31:33 +0900

    Time: 1 ms (82.76%), Space: 42 MB (50.65%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,27 +1,19 @@
 class Solution {
     public int numDecodings(String s) {
-        int size = s.length();
-        
-        int[] dp = new int[size+1];
+        int[] dp = new int[s.length()+1];
         dp[0] = 1;
         dp[1] = s.charAt(0) == '0' ? 0 : 1;
-
-        for (int i = 2; i <= size; i++) {
-            if (s.charAt(i-1) != '0')
-                dp[i] = dp[i-1];
-            if (isValid(s.substring(i-2,i))) {
+        for (int i = 2; i <= s.length(); i++) {
+            if (s.charAt(i-1) != '0') dp[i] = dp[i-1];
+            if (Integer.parseInt(s.substring(i-2,i)) >= 10 && Integer.parseInt(s.substring(i-2,i)) <= 26) {
                 dp[i] += dp[i-2];
             }
         }
-        return dp[size];
+        return dp[s.length()];
     }
-    public boolean isValid(String s) {
-        if (s.charAt(0) == '0') return false;
-        
-        int val = Integer.parseInt(s);
-        if (val <= 0 || val > 26) return false;
-
-        return true;
-    }
-
 }
+/**
+recursion -> TLE
+각 위치에서 선택지가 2개 -> DP
+
+ */
