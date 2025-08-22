4ed4bfe9f4bf4876d4fd0364d4f65a801798d3ff
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-22 10:27:40 +0900

    Time: 21 ms (44.58%), Space: 45.5 MB (21.6%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,27 +1,25 @@
 class Solution {
     public String longestPalindrome(String s) {
+        
         int max = 0;
-        String ans = new String();
-
+        String ans = "";
         for (int i = 0; i < s.length(); i++) {
             int l = i;
             int r = i;
             while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
-                if (r - l  +1 > max) {
+                if (r - l + 1 > max) {
                     max = r - l + 1;
-                    ans = s.substring(l,r + 1);
+                    ans = s.substring(l,r+1);
                 }
                 l--;
                 r++;
             }
-            
-
             l = i;
-            r = i + 1;
+            r = i+1;
             while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
-                if (r - l  +1 > max) {
+                if (r - l + 1 > max) {
                     max = r - l + 1;
-                    ans = s.substring(l,r + 1);
+                    ans = s.substring(l,r+1);
                 }
                 l--;
                 r++;
@@ -29,4 +27,5 @@ class Solution {
         }
         return ans;
     }
+    
 }
