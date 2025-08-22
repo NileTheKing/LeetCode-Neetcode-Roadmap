467f85f3e4946379f901698612e41378254856a3
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-22 09:42:15 +0900

    Time: 3 ms (23.79%), Space: 44.4 MB (24.36%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,52 +1,51 @@
 class Solution {
-    Map<Integer, Node> map = new HashMap<>();
     public int[] findRedundantConnection(int[][] edges) {
-        int n = edges.length;
-        //Map<Integer, Node> map = new HashMap<>();
-        for (int i = 1; i <= n; i++) {
-            map.put(i, new Node(i, i));
-        }
+        Map<Integer, Node> map = new HashMap<>();
         for (int[] e : edges) {
+        //map에서 n1, n2를 읽어서 없으면 만들고 있으면 조회한거 씀
+        //find먼저 해 보고 n1,n2 union함
+            if (!map.containsKey(e[0])) {
+                Node n = new Node(e[0]);
+                n.parent = n;
+                map.put(e[0], n);
+            }
+            if (!map.containsKey(e[1])) {
+                Node n = new Node(e[1]);
+                n.parent = n;
+                map.put(e[1], n);
+            }
             Node n1 = map.get(e[0]);
             Node n2 = map.get(e[1]);
-            if (union(n1, n2)) return e;
+            if (find(n1).key == find(n2).key) return new int[] {e[0], e[1]};
+            else {
+                union(n1, n2);
+            }
         }
         return null;
-        
     }
-    class Node {
-        int val;
-        int parent;
-        int rank;
+    public class Node{
+        Node parent;
+        int key;
 
-        Node(int val, int parent) {
-            this.val = val;
-            this.parent = parent;
-            this.rank = 1;
+        Node(int key) {
+            this.key = key;
         }
     }
-    public boolean union(Node n1, Node n2) {
-        Node p1 = find(n1);
-        Node p2 = find(n2);
-
-        if (p1.val == p2.val) return true;
-
-        if (p1.rank >= p2.rank) {
-            p2.parent = p1.val;
-            p1.rank += p2.rank;
-        }else {
-            p1.parent = p2.val;
-            p2.rank += p1.rank;
-        }
-        return false;
+    public Node find(Node n) { //find it's root until the end
+        Node p = n.parent;
+        if (n.key == p.key) return n;
+        else return find(p);
     }
-    public Node find(Node n) {
-        if (n.parent == n.val) 
-            return n;
-        
-        Node root = find(map.get(n.parent));
-        n.parent = root.val;
-        return root;
+    public void union(Node n1, Node n2) {
+        Node root1 = find(n1);
+        Node root2 = find(n2);
+
+        root2.parent = root1;
+        return;
     }
-    
 }
+/**
+this can be a union find problem
+or simply cycle detection problem.
+add every single cycles into a data structure then iterate thru the eges backwards.
+ */
