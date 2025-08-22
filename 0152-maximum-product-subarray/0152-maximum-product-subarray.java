014e30b995f3a0cbc2ac6e5f3e126b1fb8cbbaa5
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-22 10:19:01 +0900

    Time: 2 ms (55.19%), Space: 44.9 MB (73.24%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,20 +1,22 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int min = nums[0];
         int max = nums[0];
-        int ans = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            int cur = nums[i];
-            int tmpM = max;
+        int min = nums[0];
+        int ans = max;
+        for (int i = 1; i < nums.length; i++) { //순회하면서 max,min을 관리하며 max를 갱신. subarray여야함(consecutive)
             
-            max = Math.max(cur, 
-                Math.max(max * cur, min * cur));
-            min = Math.min(cur, 
-                Math.min(min * cur, tmpM * cur));
-
+            int temp = max;
+            max = Math.max(Math.max(nums[i], max * nums[i]),
+                            min * nums[i]);
+            min = Math.min(Math.min(nums[i], min * nums[i]),
+                            temp * nums[i]);
             ans = Math.max(ans, max);
         }
         return ans;
     }
 }
+/**
+easy as freak
+선택지가 2개. 음수냐 양수냐
+그러면 두개 관리하면 됨ㅋㅋ 어디선가 본 논리지
+ */
