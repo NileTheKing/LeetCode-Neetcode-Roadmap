9905c3386f093ce79617fd18eb64c1fb9e8fb4a6
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-25 09:22:34 +0900

    Time: 0 ms (100%), Space: 45 MB (5.62%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,14 +15,32 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
+
         return dfs(root, null, null);
     }
     public boolean dfs(TreeNode node, Integer min, Integer max) {
         if (node == null) return true;
+        
+        // int tmp_min = min == null ? Integer.MAX_VALUE : min;
+        // int tmp_max = max == null ? Integer.MIN_VALUE : max;
 
-        if (max != null && node.val <= max) return false;
+        // if (node.val < tmp_min && node.val > tmp_max) {
+        //     return dfs(node.left, node.val, max) && dfs(node.right, min, node.val);
+        // }
+        // else return false;
         if (min != null && node.val >= min) return false;
+        if (max != null && node.val <= max) return false;
 
         return dfs(node.left, node.val, max) && dfs(node.right, min, node.val);
+
+
+        
+
+
     }
 }
+/**\
+dfs로 순회하면서 검증해야해
+bst의 정의에 따라 최소와 최댓값을 가지고 다니면서 어긋나면 return false
+
+ */
