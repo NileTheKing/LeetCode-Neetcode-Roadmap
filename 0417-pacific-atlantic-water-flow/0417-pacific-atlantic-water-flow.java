2b9daad4fc41ca7f767597e1c31f032cbcaeaa22
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-26 10:43:03 +0900

    Time: 415 ms (5.02%), Space: 45.9 MB (37.34%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,47 +1,59 @@
 class Solution {
     int m, n;
+    int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         m = heights.length;
         n = heights[0].length;
-        boolean[][] pacific = new boolean[m][n]; //
+        boolean[][] pacific = new boolean[m][n];
         boolean[][] atlantic = new boolean[m][n];
-        
+
         
         for (int i = 0; i < m; i++) {
-            dfs(heights, pacific, i, 0, 0);
-            dfs(heights, atlantic, i, n-1,0);
+            System.out.println("start of pacific |");
+            dfs(heights,pacific, i, 0, -1);
+            System.out.println("end of pacific |");
+            dfs(heights,atlantic, i, n-1, -1);
         }
         for (int i = 0; i < n; i++) {
-            dfs(heights, pacific, 0, i, 0);
-            dfs(heights, atlantic, m-1, i,0);
+            dfs(heights,pacific, 0, i, -1);
+            dfs(heights,atlantic, m-1, i, -1);
         }
 
         List<List<Integer>> ans = new ArrayList<>();
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (atlantic[i][j] && pacific[i][j])  {
-                    ans.add(Arrays.asList(i,j));
-                }
+                if (pacific[i][j] && atlantic[i][j]) ans.add(Arrays.asList(i,j));
             }
         }
-        return ans;
 
+        return ans;
     }
-    public void dfs(int[][] heights, boolean[][] ocean, int r, int c, int prev) {
-        if (r < 0 || r >= m || c < 0 || c >= n || ocean[r][c] || heights[r][c] < prev) return; //미방문이 아니라면(불가능, 가능) back
-        // if (heights[r][c] < prev) {
-        //     ocean[r][c] = -1; //미방문 칸이지만 니지만 현재 칸이 유효하지 않으므로 << 섣부른 결정임.
-        //     System.out.printf("cell[%d][%d] is inavailable\n", r, c);
-        //     return;
-        // }
+    public void dfs(int[][] heights, boolean[][] ocean, int r , int c, int prevHeight){
+        // if (r < 0 || r >= m || c < 0 || c >= n || ocean[r][c]) return;
+        // if(heights[r][c] < prevHeight) return;
+        System.out.printf("visiting[%d][%d] ", r,c);
         ocean[r][c] = true;
-        dfs(heights, ocean, r + 1, c, heights[r][c]);
-        dfs(heights, ocean, r - 1, c, heights[r][c]);
-        dfs(heights, ocean, r, c + 1, heights[r][c]);
-        dfs(heights, ocean, r, c - 1, heights[r][c]);
-        
+        for (int[] d : directions) {
+            int nr = d[0] + r;
+            int nc = d[1] + c;
+            if (nr < 0 || nr >= m || nc < 0 || nc >= n ) {
+                // System.out.printf("out of bound skip. at [%d][%d]\n", nr,nc);
+                continue;
+            }
+            if (ocean[nr][nc]) {
+                //System.out.printf("visited[%d][%d] already \n", nr,nc);
+                continue;
+            }
+            if (heights[r][c] > heights[nr][nc]) {
+                //System.out.printf("cell ur going visit [%d][%d] should be higher but lower than [%d][%d]\n", nr,nc,r,c);
+                continue;
+            }
+            dfs(heights, ocean, nr, nc, heights[r][c]);
+        }
     }
 }
 /**
-모든 cell에서 하는 거 보다 edge에서 역으로 가서 겹치는 부분 return
+전체 셀에 대해 탐색하는 것 보다
+pacific, atalntic edges에서 역으로 올라가서 각각 마킹하고 겹치는 부분 찾는게 더 빠룸.
+
  */
