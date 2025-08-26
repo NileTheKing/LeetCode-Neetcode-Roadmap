8500057955e463862ee0fe1df982653762586792
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-26 09:29:56 +0900

    Time: 2 ms (99.89%), Space: 43.5 MB (22.41%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,32 +1,26 @@
 class Solution {
-    List<List<Integer>> ans;
+    List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        ans = new ArrayList<>();
-
-        dfs(candidates, target, 0, new ArrayList<>(), 0);
+        dfs(candidates, 0, target, 0, new ArrayList<>());//todo
         return ans;
     }
-    public void dfs(int[] candidates, int target, int idx, List<Integer> current, int cSum) {
-
-        if (cSum == target) {
-            ans.add(new ArrayList<>(current));
-            return;
+    public void dfs(int[] candidates, int idx, int target, int currentSum, List<Integer> path) {
+        if (currentSum == target) {
+            ans.add(new ArrayList<>(path));
         }
-        if (idx == candidates.length) return;
 
         for (int i = idx; i < candidates.length; i++) {
-            if (i > idx && candidates[i] == candidates[i-1]) continue;
-            if (cSum + candidates[i] > target) return;
-
-            current.add(candidates[i]);
-            dfs(candidates, target, i + 1, current, cSum + candidates[i]);
-            current.remove(current.size() -1);
+            if (currentSum + candidates[i] > target) return; //가망없음. 정렬되어있는 거라 뒤에 거도 다 안될거임
+            if (i > idx && candidates[i] == candidates[i -1]) continue; //뒤에 경우의 수 보기. 이미 이 경우의 수는 다 봤던 거임.
+            path.add(candidates[i]);
+            dfs(candidates, i + 1, target, currentSum + candidates[i], path);
+            path.remove(path.size() -1);
         }
-        return;
     }
 }
 /**
-1 1 2 5 6 7 10
-
+sort
+dfs backtrack
+duplicate check
  */
