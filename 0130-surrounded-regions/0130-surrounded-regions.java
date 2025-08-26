9b848ce92870f5487a7ca6549f4227351b60ceb4
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-26 11:25:00 +0900

    Time: 1 ms (99.72%), Space: 45.3 MB (72.94%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,39 +1,38 @@
 class Solution {
     int m, n;
+    int[][] directions = {{-1,0},{1,0},{0,1},{0,-1}};
     public void solve(char[][] board) {
+
         m = board.length;
-        n = board[0].length;
+        n=  board[0].length;
         for (int i = 0; i < m; i++) {
             dfs(board, i,0);
-            dfs(board, i,n-1);
+            dfs(board, i,n - 1);
         }
         for (int i = 0; i < n; i++) {
             dfs(board, 0, i);
             dfs(board, m-1, i);
         }
 
-        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == '#') board[i][j] = 'O'
-                if (board[i][j] == 'O') board[i][j] = 'X';
+                if (board[i][j] == '#') board[i][j] = 'O';
+                else {
+                    board[i][j] = 'X';
+                }
             }
         }
     }
-    public void dfs(char[][]board,  int r, int c) {
-        if (r < 0 || r >= m || c < 0 || c >= n || board[r][c] != 'O') return;
+    public void dfs(char[][] board, int r, int c) {
+        if (r < 0 || r >= m || c < 0 || c >= n || board[r][c] != 'O') {
+            //System.out.printf("passing [%d][%d]\n", r,c);
+            return;
+            } //O면 방문, 아니면 1.X는 패스 2.#는 이미 방문함
 
-        board[r][c] = '#';
-        dfs(board, r + 1, c);
-        dfs(board, r + 1, c);
-        dfs(board, r + 1, c);
-
+        board[r][c] = '#';// O를임시로 바꿔놓음
+        //System.out.printf("[%d][%d] == #\n", r, c);
+        for (int[] d : directions) {
+            dfs(board, r + d[0], c + d[1]);
+        }
     }
 }
-/**
-먹히는 거 찾아야하니까
-역으로 안먹히는 거 찾으면 됨
-    가장자리 O에서 dfs/bfs해서 연결되는 O들은 안지워짐.
-    연결되는 애들을 #로 표시하고
-    가운데(가장자리 빼고) 순회하면서 #애들은 O로 만들고 그냥 O들은 X로 바꿈
- */
