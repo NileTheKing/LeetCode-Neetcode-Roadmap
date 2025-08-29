61bcbb637453311bad963faf78102c7cfafea30e
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-08-29 09:48:04 +0900

    Time: 2 ms (86.56%), Space: 42.2 MB (78.15%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,23 +1,17 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        int length = s.length();
-        boolean[] dp = new boolean[length + 1]; //sub(0,i) 가능여부임. 
-        //length-1인덱스의 값은 dp[length] 에 담겨있음.
+        boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
-
-        for (int i = 1; i <= length; i++) {
-            for (String w : wordDict) { //i가 4일떄 leet가능ㅇ.
-                if (i - w.length() >= 0 && dp[i-w.length()] && s.substring(i - w.length(), i).equals(w)) {
-                    dp[i] = true; //그전께 됐다면 가능
+        for (int i = 1; i <= s.length(); i++) {
+            for (String word : wordDict) {
+                int length = word.length(); // for index calc
+                if (i - length >= 0 && dp[i-length] && s.substring(i-length,i).equals(word)) {
+                    dp[i] = true;
+                    //System.out.printf("at dp[%d]: substring %s + %s makes up %s\n", i, s.substring(0,i-length), s.substring(i-length, i), s.substring(0, i));
                     break;
-                    //System.out.println("sub(" + (i-w.length()) + ", " + i +") updated dp["+i+"]");
-                    //System.out.println("current word:" + w);
-                    //System.out.println("trying to make: " + s.substring(i-w.length(), i));
-    
                 }
             }
         }
-
-        return dp[length];
+        return dp[s.length()];
     }
 }
