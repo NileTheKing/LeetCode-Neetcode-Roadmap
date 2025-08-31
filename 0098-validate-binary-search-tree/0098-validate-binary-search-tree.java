3035f07e70905c1f2617382f932bfc6105ababc3
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-09-01 08:57:59 +0900

    Time: 0 ms (100%), Space: 44.6 MB (13.57%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,32 +15,20 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-
-        return dfs(root, null, null);
+        return validate(root, null, null);    
     }
-    public boolean dfs(TreeNode node, Integer min, Integer max) {
+    public boolean validate(TreeNode node, Integer max, Integer min) {
         if (node == null) return true;
         
-        // int tmp_min = min == null ? Integer.MAX_VALUE : min;
-        // int tmp_max = max == null ? Integer.MIN_VALUE : max;
-
-        // if (node.val < tmp_min && node.val > tmp_max) {
-        //     return dfs(node.left, node.val, max) && dfs(node.right, min, node.val);
-        // }
-        // else return false;
-        if (min != null && node.val >= min) return false;
         if (max != null && node.val <= max) return false;
-
-        return dfs(node.left, node.val, max) && dfs(node.right, min, node.val);
-
+        if (min != null && node.val >= min) return false;
 
         
-
-
+        return validate(node.left, max, node.val) && validate(node.right, node.val, min);
     }
 }
-/**\
-dfs로 순회하면서 검증해야해
-bst의 정의에 따라 최소와 최댓값을 가지고 다니면서 어긋나면 return false
+/**
+A left subtree of a node should be stricly less than the node's val
+and right side..
 
  */
