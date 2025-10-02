70e82bffa0bb87f5cd220a897e7f12ae4798287c
양나일, Nile Yang <nileyangdev@gmail.com>
2025-10-02 15:58:47 +0900

    Time: 7 ms (26.14%), Space: 46.1 MB (54.46%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,37 +1,34 @@
 class Solution {
-    List<Integer> ans = new ArrayList<>();
-    Map<Integer, List<Integer>> map = new HashMap<>();
-    Set<Integer> visited = new HashSet<>();
+    Map<Integer, List<Integer>> map;
+    Set<Integer> visited;
+    List<Integer> ans;
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        
+        map = new HashMap<>();
+        visited = new HashSet<>();
+        ans = new ArrayList<>();
+
         for (int[] p : prerequisites) {
-            map.computeIfAbsent(p[0], k-> new ArrayList<>()).add(p[1]);
+            map.computeIfAbsent(p[0], v -> new ArrayList<>()).add(p[1]);
         }
 
         for (int i = 0; i < numCourses; i++) {
-            if (visited.contains(i)) continue;
-            if (!dfs(i, new HashSet<>())) return new int[0];
+            if (takeCourse(prerequisites, i, new HashSet<>())) return new int[0];
         }
+
         return ans.stream().mapToInt(i->i).toArray();
     }
-    public boolean dfs(int current, Set<Integer> visiting) {
-        if (visiting.contains(current)) return false;
-        if (visited.contains(current)) return true;
+    //if true it has cycle
+    public boolean takeCourse(int[][] prerequisites, int current, Set<Integer> visiting) {
+        if (visiting.contains(current)) return true;
+        if (visited.contains(current)) return false;
 
         visiting.add(current);
         for (int nei : map.getOrDefault(current, new ArrayList<>())) {
-            if (!dfs(nei, visiting)) return false;
+            if (takeCourse(prerequisites, nei, visiting)) return true;
         }
-        
-        visiting.remove(current);
         visited.add(current);
+        visiting.remove(current);
         ans.add(current);
-        return true;
+        return false;
     }
 }
-/**
-백트래킹과 그래프 순회
-일단 순서는 상관없으니 모든 지점에서 entry해야함
-그리고 음 뒤에서부터 순서를 dfs에서 넣어야하는데 그거를 어떻게하지?
-list에 추가하면 되겟네
- */
