0353c37f7c77f0bd26001b9672a12bad5ff942fe
양나일, Nile Yang <nileyangdev@gmail.com>
2025-10-02 15:43:44 +0900

    Time: 7 ms (44.22%), Space: 46.2 MB (8.37%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,39 +1,39 @@
 class Solution {
-    Map<Integer, List<Integer>> map = new HashMap<>();
-    Set<Integer> visited = new HashSet<>();
+    Map<Integer, List<Integer>> map;
+    Set<Integer> visited;
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-
-        for (int[] pr : prerequisites) {//graph 완성
-            map.computeIfAbsent(pr[0], k-> new ArrayList<>()).add(pr[1]);
-        }      
-
-        //모든 노드를 순회하며 사이클 확인. 방향그래프므로 parent없이 그냥 방문중인 노드 발견하면 cycle.
-        for (int i = 0; i < numCourses; i++) {
-            if (!visited.contains(i) && hasCycle(new HashSet<>(), i)) {
-                // System.out.printf("%d has cycle", i);
-                return false;
-            }
+        map = new HashMap<>();
+        visited = new HashSet<>();
+        for (int[] pre : prerequisites) {
+            int v1 = pre[0];
+            int v2 = pre[1];
+            map.computeIfAbsent(v1, v -> new ArrayList<>()).add(v2);
         }
-        return true;
+
+        //multiple entry points
+        for (int i = 0; i < numCourses; i++) {
+            if (hasCycle(prerequisites, i, new HashSet<>())) return false;
+        }
+
+        return visited.size() == numCourses;
     }
-    public boolean hasCycle(Set<Integer> visiting, int current) {
+
+    public boolean hasCycle(int[][] prerequisites, int current, Set<Integer> visiting) {
         if (visiting.contains(current)) {
-            // System.out.print("has been visitng: ");
-            // for (Integer i : visiting) {
-            //     System.out.printf("%d, ", i);
-            // }
-            // System.out.printf("and %d is the problem \n", current);
             return true;
         }
         if (visited.contains(current)) return false;
 
         visiting.add(current);
-
-        for(int nei : map.getOrDefault(current, new ArrayList<>())) {
-            if (hasCycle(visiting, nei)) return true;
+        for (int nei : map.getOrDefault(current, new ArrayList<>())) {
+            if (visited.contains(nei)) continue;//이미 방문했던 애들은 안함
+            if (hasCycle(prerequisites, nei, visiting)) return true;
         }
         visiting.remove(current);
         visited.add(current);
         return false;
     }
-}
+}/**
+if there is a cycle int the graph, return false
+from multiple entries, check if there is a cycle made thru the travesal
+ */
