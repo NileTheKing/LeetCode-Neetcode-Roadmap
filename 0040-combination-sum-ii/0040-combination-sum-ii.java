be203101964dc1c10089c727bd48b4e4309c781b
양나일, Nile Yang <nileyangdev@gmail.com>
2025-10-04 17:17:20 +0900

    Time: 5 ms (15.02%), Space: 43.4 MB (36.46%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -2,25 +2,23 @@ class Solution {
     List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        dfs(candidates, 0, target, 0, new ArrayList<>());//todo
+        backtrack(candidates, 0, target, 0, new ArrayList<>());
+
         return ans;
     }
-    public void dfs(int[] candidates, int idx, int target, int currentSum, List<Integer> path) {
-        if (currentSum == target) {
+    public void backtrack(int[] candidates, int current, int target, int idx, List<Integer> path) {
+        if (current == target) {
             ans.add(new ArrayList<>(path));
         }
+        if (current > target) return;
 
         for (int i = idx; i < candidates.length; i++) {
-            if (currentSum + candidates[i] > target) return; //가망없음. 정렬되어있는 거라 뒤에 거도 다 안될거임
-            if (i > idx && candidates[i] == candidates[i -1]) continue; //뒤에 경우의 수 보기. 이미 이 경우의 수는 다 봤던 거임.
+            if (i > idx && candidates[i] == candidates[i - 1]) continue;
+            current += candidates[i];
             path.add(candidates[i]);
-            dfs(candidates, i + 1, target, currentSum + candidates[i], path);
-            path.remove(path.size() -1);
+            backtrack(candidates, current, target, i + 1, path);
+            current -= candidates[i];
+            path.remove(path.size() - 1);
         }
     }
 }
-/**
-sort
-dfs backtrack
-duplicate check
- */
