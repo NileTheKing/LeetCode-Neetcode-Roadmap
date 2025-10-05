38cb34bc14d3bfad32d891683d39f8641219a4ad
양나일, Nile Yang <nileyangdev@gmail.com>
2025-10-06 00:13:03 +0900

    Time: 44 ms (23.27%), Space: 51.8 MB (34.94%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,36 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-        for (int l =  0; l < nums.length; l++) {
-            if (l > 0 && nums[l] == nums[l-1]) continue;
+        for (int l = 0; l < nums.length - 2; l++) {
+            if (l > 0 && nums[l] == nums[l -1]) continue;
+            //각 l에 대해 투포인터 진행
             int m = l + 1;
-            int r = nums.length - 1;
-            int target = -nums[l];// sum that m and r should make up
-            while (m <= nums.length -1 && m < r) {
-                int sum = nums[m] + nums[r];
-                //System.out.printf("nums[%d] + nums[%d] = %d\n", m,r,sum);
-                if (sum > target) {
-                    r--;
-                }else if(sum < target) {
-                    m++;
-                }else { //일단 합이 되면 추가는 해야되고 m도 올려줘야하고..
-                    //System.out.printf("l,m,r: %d %d %d\n", l,m,r);
-                    if (m > l + 1 && nums[m] == nums[m - 1]) {
-                        //System.out.printf("passing. l = %d, m = %d, r = %d\n", l,m,r);
-                        m++; continue;
-                    }
-                    else {ans.add(Arrays.asList(nums[l],nums[m], nums[r]));
-                    m++;
-                    }
+            int r  = nums.length - 1;
+            if (m >= nums.length - 1 || r >= nums.length) break;
+            // nums[i] + nums[m]+ nums[r] == 0 -> 
+            int target_mr = -nums[l];
+            while (m < r) {
+                if (m > l + 1 && nums[m] == nums[m-1]) {m++; continue;}
+                if (r < nums.length - 1 && nums[r] == nums[r + 1]) {r--; continue;}
+                int num_m = nums[m];
+                int num_r = nums[r];
+                // System.out.printf("l,m,r: %d %d %d\n", l, m, r);
+                // System.out.printf("nums[l] == %d, target m+r= %d\n", nums[l], target_mr);
+                // System.out.printf("current mr sum = %d\n", num_m + num_r);
+                
+                //일치
+                if (num_m + num_r == target_mr) {
+                        ans.add(Arrays.asList(nums[l],num_m,num_r));
+                        m++;
                 }
+                //합한게 너무 작음
+                else if (num_m + num_r < target_mr) m++;
+                //합한게 더 큼
+                else r--;
             }
         }
         return ans;
     }
 }
-/**
-l + m + r = 0
- */
