bd15d57dd22e1f0810161ae7b6b6d18c5c5ef506
양나일, Nile Yang <nileyangdev@gmail.com>
2025-10-06 22:07:30 +0900

    Time: 1 ms (60.37%), Space: 46.2 MB (68.24%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,33 +2,26 @@ class Solution {
     public int trap(int[] height) {
         int[] leftMax = new int[height.length];
         int[] rightMax = new int[height.length];
-
+        int currentLeftMax = 0;
         leftMax[0] = 0;
-        leftMax[1] = height[0];
-        for (int i = 2; i < height.length; i++) {
-            leftMax[i] = Math.max(leftMax[i - 1], height[i - 1]);
-            //System.out.printf("leftMax[%d] == %d\n", i, leftMax[i]);
+        for (int i = 1; i < height.length; i++) {
+            leftMax[i] = Math.max(currentLeftMax, height[i-1]);
+            currentLeftMax = Math.max(currentLeftMax, height[i-1]);
+            //System.out.printf("leftMax[%d] = %d, currentLeftMax = %d\n", i, leftMax[i], currentLeftMax);
+        }
+        int currentRightMax = 0;
+        rightMax[height.length - 1] = 0;
+        for (int i = height.length - 1 - 1; i >= 0; i--) {
+            rightMax[i] = Math.max(currentRightMax, height[i+1]);
+            currentRightMax = Math.max(currentRightMax, height[i+1]);
+            //System.out.printf("rightMax[%d] = %d, currentRightMax = %d\n", i, rightMax[i], currentRightMax);
         }
 
-        rightMax[height.length - 1] = 0;
-        rightMax[height.length - 1 - 1] = height[height.length - 1];
-        for (int i = 2; i < height.length; i++) {
-            //leftMax[i] = Math.max(leftMax[i - 1], height[i - 1]);
-            rightMax[height.length - 1 - i] = Math.max(rightMax[height.length - 1 - i + 1], height[height.length - 1 - i + 1]);
-            //System.out.printf("rightMax[%d] == %d\n", height.length -1 - i, rightMax[height.length - 1 - i]);
-        }
-        // for (int i = 0; i < height.length; i++) {
-        //     System.out.printf("current cell %d's leftMax: %d, rightMax: %d\n", i, leftMax[i], rightMax[i]);
-        // }
-        int ans = 0;
+        int sum = 0;
         for (int i = 1; i < height.length; i++) {
             int diff = Math.min(leftMax[i], rightMax[i]) - height[i];
-            if (diff > 0){
-                ans += diff;
-                //System.out.printf("current space %d holds %d water\n",i, diff);
-            }
-
+            if (diff > 0) sum += diff;
         }
-        return ans;
+        return sum;
     }
 }
