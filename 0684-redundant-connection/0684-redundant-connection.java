aedd531a9a896ae448005cf8a978bcc1680cad55
양나일, Nile Yang <nileyangdev@gmail.com>
2025-10-07 11:01:33 +0900

    Time: 762 ms (5.12%), Space: 46.3 MB (6.34%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,44 +1,60 @@
 class Solution {
     Map<Integer, List<Integer>> map = new HashMap<>();
-    Set<Integer> cycle = new HashSet<>();
-    Set<Integer> visited = new HashSet<>();
+    Set<Vertice> cycles = new HashSet<>();
+    //Set<Integer> visited = new HashSet<>();
     public int[] findRedundantConnection(int[][] edges) {
         for (int[] e : edges) {
             map.computeIfAbsent(e[0], k -> new ArrayList<>()).add(e[1]);
             map.computeIfAbsent(e[1], k -> new ArrayList<>()).add(e[0]);
-
-            findCycles(e[0], -1, new HashSet<>());
-            if(cycle.contains(e[0]) && cycle.contains(e[1])) {
-                return e;
-            }
         }
-        
 
+        for (int i = 1; i < edges.length; i++) {
+            hasCycle(edges[i][0], -1, new HashSet<>());
+        }
+        for (int i = edges.length - 1; i >= 0; i--) {
+            if (cycles.contains(new Vertice(edges[i][0], edges[i][1])) || cycles.contains(new Vertice(edges[i][1], edges[i][0]))) return new int[] {edges[i][0], edges[i][1]};
+        }
 
         return new int[0];
     }
-    public void findCycles(int current, int parent, Set<Integer> visiting) {
+    public void hasCycle(int current, int parent, Set<Integer> visiting) {
         if (visiting.contains(current)) {
-            for (int n : visiting) {
-                cycle.add(n);
-                //System.out.printf("cycle added: %d\n", n);
-            }
+            cycles.add(new Vertice(current, parent));
+            //System.out.printf("%d %d cycle\n", current, parent);
             return;
         }
         //if (visited.contains(current)) return;
         
-        System.out.printf("visiting %d \n", current);
+        //System.out.printf("%d to %d\n", parent, current);
         visiting.add(current);
-        for (int nei : map.getOrDefault(current, new ArrayList<>())) {
-            if (nei != parent) {
-                findCycles(nei, current, visiting);
-            }
+        
+        for (int nei : map.getOrDefault(current, List.of())) {
+            if (nei != parent) hasCycle(nei, current, visiting);
         }
         visiting.remove(current);
-        visited.add(current);
+        //visited.add(current);
+
         return;
     }
+    public class Vertice {
+        int v1;
+        int v2;
+
+        Vertice(int v1, int v2) {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Vertice v = (Vertice) o;
+            return this.v1 == v.v1 && this.v2 == v.v2;
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(v1, v2);
+        }
+    }
 }
-/**
-cycle detection으로 가자. cycle ds에  넣어두고 edges input을 뒤로부터 실행해서 싸이클
- */
