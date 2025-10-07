2d665622a54098db7b49feb43ef045aa55897b19
양나일, Nile Yang <nileyangdev@gmail.com>
2025-10-07 16:42:07 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,60 +1,35 @@
 class Solution {
     Map<Integer, List<Integer>> map = new HashMap<>();
-    Set<Vertice> cycles = new HashSet<>();
-    //Set<Integer> visited = new HashSet<>();
+    //Set<Integer> cycle = new HashSet<>();
+    //Set<Vertice> cycles = new HashSet<>();
+    Set<Integer> visited = new HashSet<>();
     public int[] findRedundantConnection(int[][] edges) {
         for (int[] e : edges) {
             map.computeIfAbsent(e[0], k -> new ArrayList<>()).add(e[1]);
             map.computeIfAbsent(e[1], k -> new ArrayList<>()).add(e[0]);
-        }
 
-        for (int i = 1; i < edges.length; i++) {
-            hasCycle(edges[i][0], -1, new HashSet<>());
-        }
-        for (int i = edges.length - 1; i >= 0; i--) {
-            if (cycles.contains(new Vertice(edges[i][0], edges[i][1])) || cycles.contains(new Vertice(edges[i][1], edges[i][0]))) return new int[] {edges[i][0], edges[i][1]};
+            if (visited.contains(e[0]) && visited.contains(e[1])) return e;
+            traverse(e[0], -1, new HashSet<>());
+            traverse(e[1], -1, new HashSet<>());
         }
 
         return new int[0];
     }
-    public void hasCycle(int current, int parent, Set<Integer> visiting) {
+    public void traverse(int current, int parent, Set<Integer> visiting) {
         if (visiting.contains(current)) {
-            cycles.add(new Vertice(current, parent));
-            //System.out.printf("%d %d cycle\n", current, parent);
             return;
         }
-        //if (visited.contains(current)) return;
+        if (visited.contains(current)) return;
         
         //System.out.printf("%d to %d\n", parent, current);
         visiting.add(current);
         
         for (int nei : map.getOrDefault(current, List.of())) {
-            if (nei != parent) hasCycle(nei, current, visiting);
+            if (nei != parent) traverse(nei, current, visiting);
         }
         visiting.remove(current);
-        //visited.add(current);
+        visited.add(current);
 
         return;
-    }
-    public class Vertice {
-        int v1;
-        int v2;
-
-        Vertice(int v1, int v2) {
-            this.v1 = v1;
-            this.v2 = v2;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Vertice v = (Vertice) o;
-            return this.v1 == v.v1 && this.v2 == v.v2;
-        }
-        @Override
-        public int hashCode() {
-            return Objects.hash(v1, v2);
-        }
     }
 }
