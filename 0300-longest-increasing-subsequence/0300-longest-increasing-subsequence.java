84c62949c5387a3043a512af24d798c196b774e8
양나일, Nile Yang <nileyangdev@gmail.com>
2025-10-08 20:51:23 +0900

    Time: 49 ms (39.68%), Space: 44.1 MB (88.87%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -2,20 +2,14 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         int[] dp = new int[nums.length];
         Arrays.fill(dp, 1);
-        for (int i = 0; i < nums.length; i++) {
+        //dp[i] 는 i전까지 길이? 이거는 근데 점화식이 딱히.. 1이니까 할 필요가 없지 않나?
+
+        for (int i = 0; i < nums.length; i++) { //i는 nums인덱스
             for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    // System.out.printf("i:%d, j:%d ", i, j);
-                    // System.out.printf("dp[%d] = Math.max(%d + 1, %d)\n", i, dp[j], dp[i]);
-                    dp[i] = Math.max(dp[j] + 1, dp[i]);
-                }
+                //System.out.printf("i,j: %d %d\n", i,j);
+                if (nums[i] > nums[j]) dp[i] = Math.max(dp[j] + 1, dp[i]);
             }
         }
         return Arrays.stream(dp).max().getAsInt();
     }
 }
-/**
-문제를 분해가능
-    각 인덱스에서 포함시킬지 안포함시킬지로.
-        -> 둘다 관리하면 된다
- */
