0e704bbb8fd2a5bf84e68b4be2b88c89e4fc116c
양나일, Nile Yang <nileyangdev@gmail.com>
2025-10-09 18:43:25 +0900

    Time: 15 ms (77.72%), Space: 44.3 MB (82.02%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,24 +1,25 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if (amount == 0) return 0;
-
+        int[] dp = new int[amount+1];// dp[i] means the number of coins to make up i
+        Arrays.fill(dp,amount+1); //초기화
         Arrays.sort(coins);
-        if (coins[0] > amount) return -1;
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
         for (int coin : coins) {
-            if (coin <= amount)
-                dp[coin] = 1;
+            if (coin > amount) break;
+            dp[coin] = 1;
         }
+        dp[0] = 0;
 
         for (int i = 1; i <= amount; i++) {
             for (int coin : coins) {
-                if (i - coin > 0) {
-                    dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
-                }
+                if (i - coin < 0) continue;
+                dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
+                //System.out.printf("dp[%d] = Math.min(%d,%d)\n", i, dp[i-coin] + 1, dp[i]);
             }
         }
 
         return dp[amount] > amount ? -1 : dp[amount];
     }
 }
+/**
+
+ */
