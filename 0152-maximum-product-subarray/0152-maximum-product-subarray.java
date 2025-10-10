40ac85e740c189ca84e058b047fc00e570eda686
양나일, Nile Yang <nileyangdev@gmail.com>
2025-10-10 20:34:20 +0900

    Time: 3 ms (18.38%), Space: 45.6 MB (7.59%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,22 +1,15 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        
-        int ans = nums[0];
         int min = nums[0];
         int max = nums[0];
+        int ans = max;
         for (int i = 1; i < nums.length; i++) {
-            int tmpMax = max;
-            max = Math.max(Math.max(max * nums[i], nums[i]), min * nums[i]);
-            min = Math.min(Math.min(tmpMax * nums[i], nums[i]), min * nums[i]);
+            int tempMin=min;
+            min = Math.min(Math.min(min * nums[i], nums[i]), max * nums[i]);
+            max = Math.max(Math.max(max * nums[i], nums[i]), tempMin * nums[i]);
             ans = Math.max(ans, max);
-            //System.out.printf("at index:%d, max=%d, min=%d, ans=%d", i, max, min, ans);
         }
         return ans;
+
     }
 }
-/**
-음수면 문제가 생긴다
-각 인덱스에 해당하는 숫자가 양수면 최댓값이랑 곱해서 업데이트
-음수도 따로 관리를 해둬야함. 
-
- */
