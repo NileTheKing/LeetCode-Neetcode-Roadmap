34e80265f2953fb00e4c14c326d4fa15381d6213
양나일, Nile Yang <nileyangdev@gmail.com>
2025-10-10 20:18:32 +0900

    Time: 5 ms (63.52%), Space: 43 MB (64.73%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,15 +1,13 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        boolean[] dp = new boolean[s.length() + 1];
+        boolean[] dp = new boolean[s.length() + 1]; //dp[i]는 s의 i인덱스 이전까지 된다 이뜻
         dp[0] = true;
+
         for (int i = 1; i <= s.length(); i++) {
-            for (String word : wordDict) {
-                int length = word.length(); // for index calc
-                if (i - length >= 0 && dp[i-length] && s.substring(i-length,i).equals(word)) {
+            for (String w : wordDict) {
+                int length = w.length();
+                if (i - length >= 0 && dp[i-length] && s.substring(i-length, i).equals(w)) //i-length-1인덱스까지 되는거면 현재 dp[i]즉 s에서 i-1인덱스까지 되냐보는거
                     dp[i] = true;
-                    //System.out.printf("at dp[%d]: substring %s + %s makes up %s\n", i, s.substring(0,i-length), s.substring(i-length, i), s.substring(0, i));
-                    break;
-                }
             }
         }
         return dp[s.length()];
