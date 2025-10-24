5a9463a6fcebe53dee2fdfd40c71357fc8448945
양나일, Nile Yang <nileyangdev@gmail.com>
2025-10-24 10:57:26 +0900

    Time: 27 ms (29.05%), Space: 42.9 MB (55.85%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -21,32 +21,28 @@ class Node {
 class Solution {
     public Node cloneGraph(Node node) {
         if (node == null) return null;
-        Queue<Node> q = new LinkedList<>();
-        q.offer(node);
-        Map<Node, Node> map = new HashMap<>();
-        map.put(node, new Node(node.val));
-        Set<Node> visited = new HashSet<>();
-        while (!q.isEmpty() && q.peek() != null) {//원본을 꺼내서 복사본을
-            Node current = q.poll();
-            //map.put(current, new Node(current.val));
-            Node copy = map.get(current);
-            List<Node> copiedNeighbors = new ArrayList<>();
-            visited.add(current);
-            for (Node nei : current.neighbors) {
-                map.computeIfAbsent(nei, k -> new Node(nei.val));
-                if (!visited.contains(nei)) q.offer(nei);
-                copiedNeighbors.add(map.get(nei));
+        Map<Node, Node> map = new HashMap<>(); //진,짭
+        Node current = node;//찐 진입점
+        Queue<Node> q = new LinkedList<>(); //찐 방문할 노드들
+        Set<Integer> visited = new HashSet<>(); //이미 방문했던 노드들
+        q.offer(current);
+        map.put(current, new Node(current.val));
+        visited.add(current.val);
+        while(!q.isEmpty()) {
+            Node polled = q.poll();
+            Node copy = map.get(polled);
+            //System.out.printf("copying %d\n", polled.val);
+            List<Node> copiedNei = new ArrayList<>();
+            for (Node nei : polled.neighbors) {
+                if (!visited.contains(nei.val)) {
+                    visited.add(nei.val);    
+                    q.offer(nei);
+                }
+                if(!map.containsKey(nei)) map.put(nei, new Node(nei.val));
+                copiedNei.add(map.get(nei));
             }
-            copy.neighbors = copiedNeighbors;
+            copy.neighbors = copiedNei;
         }
         return map.get(node);
     }
 }
-/**
-원래대로면 짝을 다 지어주고 그다음에 순회하면서 복사
-복사: 
-    카피를 map.get해서 꺼내오고
-    거기의 neighbors<< 얘를 이제 기존 원본의 neighbors를 돌면서 map.get해서 하나씩 추가
-    그로 마지막으로 리스트를 대입.
-마무리.
- */
