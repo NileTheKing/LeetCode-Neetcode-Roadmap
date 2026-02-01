c189c19a2385a4d42f3f4b768e453ba2519b0a47
양나일, Nile Yang <nileyangdev@gmail.com>
2026-02-01 20:33:34 +0900

    Time: 3 ms (25.89%), Space: 45.4 MB (25.26%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,35 +1,60 @@
 class Solution {
-    Map<Integer, List<Integer>> map = new HashMap<>();
-    //Set<Integer> cycle = new HashSet<>();
-    //Set<Vertice> cycles = new HashSet<>();
-    Set<Integer> visited = new HashSet<>();
+    Map<Integer, Node> map = new HashMap<>(); //매핑
     public int[] findRedundantConnection(int[][] edges) {
+        
+        //초기값
         for (int[] e : edges) {
-            map.computeIfAbsent(e[0], k -> new ArrayList<>()).add(e[1]);
-            map.computeIfAbsent(e[1], k -> new ArrayList<>()).add(e[0]);
+            if (!map.containsKey(e[0])) {
+                Node node = new Node(e[0]);
+                node.parent = node;
+                map.put(e[0], node);
+            }
+            if (!map.containsKey(e[1])) {
+                Node node = new Node(e[1]);
+                node.parent = node;
+                map.put(e[1], node);
+            }
+        }
 
-            if (visited.contains(e[0]) && visited.contains(e[1])) return e;
-            traverse(e[0], -1, new HashSet<>());
-            traverse(e[1], -1, new HashSet<>());
+
+        //순회하면서 합치는데 언제 안합치나? 둘의 부모가 같으면 .. 다
+        for (int[] e : edges) {
+            Node root1 = find(map.get(e[0]));
+            Node root2 = find(map.get(e[1]));
+
+            //root값이 동일 -> 이미 연결되어있음
+            if (root1.val == root2.val) return new int[] {e[0], e[1]};
+            else { //아니라면 합치고 계속
+                union(root1, root2);
+            }
         }
 
         return new int[0];
-    }
-    public void traverse(int current, int parent, Set<Integer> visiting) {
-        if (visiting.contains(current)) {
-            return;
-        }
-        if (visited.contains(current)) return;
-        
-        //System.out.printf("%d to %d\n", parent, current);
-        visiting.add(current);
-        
-        for (int nei : map.getOrDefault(current, List.of())) {
-            if (nei != parent) traverse(nei, current, visiting);
-        }
-        visiting.remove(current);
-        visited.add(current);
 
+    }
+    public class Node {
+        int val;
+        Node parent;
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public Node find(Node n) {
+        
+        if (n.val == n.parent.val) return n;
+        else return find(n.parent);
+    }
+    public void union(Node n1, Node n2) { //n1에 n2를 병합
+        Node root1 = find(n1);
+        Node root2 = find(n2);
+
+        root2.parent = root1;
         return;
     }
+
+    /**
+    
+    unionfind에서 사이클 나오는 경우?
+     */
 }
