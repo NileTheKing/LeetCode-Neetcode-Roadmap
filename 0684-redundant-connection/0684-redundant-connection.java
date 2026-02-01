3c247a916908d995749105d430f3ee7f30fa43b3
양나일, Nile Yang <nileyangdev@gmail.com>
2026-02-01 20:47:21 +0900

    Time: 6 ms (14.09%), Space: 45.6 MB (23.41%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -2,48 +2,36 @@ class Solution {
     public int[] findRedundantConnection(int[][] edges) {
         Map<Integer, Node> map = new HashMap<>();
         for (int[] e : edges) {
-            if (!map.containsKey(e[0])) {
-                Node n1 = new Node(e[0]);
-                n1.parent = n1;
-                map.put(e[0], n1);
-            }
-            if (!map.containsKey(e[1])) {
-                Node n2 = new Node(e[1]);
-                n2.parent = n2;
-                map.put(e[1], n2);
-            }
+            map.computeIfAbsent(e[0], k -> new Node(k));
+            map.computeIfAbsent(e[1], k -> new Node(k));
 
-            if (find(map.get(e[1])).val == find(map.get(e[0])).val) {
-                return new int[] {e[0], e[1]};
+            Node root1 = find(map.get(e[0]));
+            Node root2 = find(map.get(e[1]));
+            if (root1.val == root2.val) {
+                return new int[]{e[0], e[1]};
             }else {
-                union(map.get(e[0]), map.get(e[1]));
+                union(root1, root2);
             }
         }
         return new int[0];
-
     }
     public class Node {
         int val;
         Node parent;
         Node(int val) {
             this.val = val;
+            this.parent = this;
         }
     }
-    public void union(Node n1, Node n2) { 
-        Node p2 = find(n2);
-        Node p1 = find(n1);
-        p2.parent = p1;
+    public void union(Node n1, Node n2) {
+        n2.parent = n1;
         return;
     }
     public Node find(Node n) {
         if (n.val == n.parent.val) return n;
         else {
-            n.parent = find(n.parent);
+            n.parent = find(n.parent);//path compression
             return n.parent;
         }
     }
 }
-/**
-1. union find
-2. cycle detection (dfs)
- */
