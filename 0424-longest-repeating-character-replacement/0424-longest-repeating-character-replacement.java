dced090d6cd036ae551b78234212b6367dfa344c
양나일, Nile Yang <nileyangdev@gmail.com>
2026-02-02 12:19:15 +0900

    Time: 6 ms (98.63%), Space: 46.1 MB (88.25%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,29 +1,21 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        
+        int len = s.length();
         int l = 0;
         int[] freq = new int[26];
         int mostFreq = 0;
-        int ans = 0;
-        for (int r = 0; r < s.length(); r++) {
+        int max = 0;
+        for (int r = 0; r < len; r++) {
             char c = s.charAt(r);
-            freq[c-'A']++;
+            freq[c - 'A']++;
             mostFreq = Math.max(mostFreq, freq[c-'A']);
-            while (r - l + 1 - mostFreq > k) { //길이에서 최빈값 제외하면 봐줄 수 있는 갯수. k보다 작으면 안됨.
+            
+            while (r - l + 1 - mostFreq > k) {
                 freq[s.charAt(l) - 'A']--;
                 l++;
             }
-            ans = Math.max(ans, r - l + 1);
+            max = Math.max(max, r - l + 1);
         }
-
-        return ans;
+        return max;
     }
 }
-/**
-substring -> two pointers
-right keeps going anyways, it's a matter of l following or not
-it can expand(l stays at the same pos). if it's allowed to expand(k restriction).
-you need to keep track of the most frequent character and you don't have to know what that is.
-    bcuz ????? <- mola
-    but you def need to know the frequency.
- */
