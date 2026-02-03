447bc2e2697f00120fb368b938d4b1af0706be76
양나일, Nile Yang <nileyangdev@gmail.com>
2026-02-03 17:31:54 +0900

    Time: 28 ms (13.71%), Space: 44.7 MB (5.84%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -21,37 +21,28 @@ class Node {
 class Solution {
     public Node cloneGraph(Node node) {
         if (node == null) return null;
-        Map<Node, Node> weave = new HashMap<>();
-        weave.put(node, new Node(node.val));
         Queue<Node> q = new LinkedList<>();
+        Map<Node, Node> map = new HashMap<>();
         Set<Integer> visited = new HashSet<>();
-        visited.add(node.val);
         q.offer(node);
-        while(!q.isEmpty()) {
-            Node current = q.poll();
-            Node copiedNode = weave.get(current);
-            //System.out.printf("%d done\n", current.val);
-            List<Node> copiedNeighbors = new ArrayList<>();
-
+        map.put(node, new Node(node.val));
+        visited.add(node.val);
+        while (!q.isEmpty()) {
+            Node poll = q.poll();
+            Node copied = map.get(poll);
+            // System.out.printf("copying %d\n", poll.val);
+            List<Node> copiedNei = new ArrayList<>();
             
-            for (Node nei : current.neighbors) {
+            for (Node nei : poll.neighbors) {
                 if (!visited.contains(nei.val)) {
-                    q.offer(nei);
                     visited.add(nei.val);
+                    q.offer(nei);
                 }
-                //if (weave.containsKey(nei)) weave.put(nei,new Node(nei.val));
-                if (!weave.containsKey(nei)) weave.put(nei, new Node(nei.val));
-                copiedNeighbors.add(weave.get(nei));
+                map.computeIfAbsent(nei, k -> new Node(k.val));
+                copiedNei.add(map.get(nei));
             }
-            
-            copiedNode.neighbors = copiedNeighbors;
-            //visited.add(current.val);
+            copied.neighbors = copiedNei;
         }
-        return weave.get(node);
+        return map.get(node);
     }
 }
-
-/**
-원래 생각: 미리 다 위빙을 해두고 원본 따라가면서 복사본을 연결해주기
-1go    : 매번 위빙하면서 따라가면서 복사뜨고 따라가는 애 복사본 이웃추가.
- */
