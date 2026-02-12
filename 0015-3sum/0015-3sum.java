04c119f3ebb84e23172a61b51d08100f19b25d46
양나일, Nile Yang <nileyangdev@gmail.com>
2026-02-13 01:24:17 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,28 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-        for (int l = 0; l < nums.length - 2; l++) {
-            if (l > 0 && nums[l] == nums[l -1]) continue;
-            //각 l에 대해 투포인터 진행
-            int m = l + 1;
-            int r  = nums.length - 1;
-            if (m >= nums.length - 1 || r >= nums.length) break;
-            // nums[i] + nums[m]+ nums[r] == 0 -> 
-            int target_mr = -nums[l];
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i -1]) continue;
+            int l = i;
+            int m = i + 1;
+            int r = nums.length - 1;
+            int target = -nums[l];
             while (m < r) {
-                if (m > l + 1 && nums[m] == nums[m-1]) {m++; continue;}
-                if (r < nums.length - 1 && nums[r] == nums[r + 1]) {r--; continue;}
-                int num_m = nums[m];
-                int num_r = nums[r];
-                // System.out.printf("l,m,r: %d %d %d\n", l, m, r);
-                // System.out.printf("nums[l] == %d, target m+r= %d\n", nums[l], target_mr);
-                // System.out.printf("current mr sum = %d\n", num_m + num_r);
-                
-                //일치
-                if (num_m + num_r == target_mr) {
-                        ans.add(Arrays.asList(nums[l],num_m,num_r));
-                        m++;
+                if (m > l + 1 && nums[m] == nums[m - 1]) {
+                    m++;
+                    continue;
                 }
-                //합한게 너무 작음
-                else if (num_m + num_r < target_mr) m++;
-                //합한게 더 큼
-                else r--;
+                int sum_mr = nums[m] + nums[r];
+                if (sum_mr == target) {
+                    // System.out.printf("sum_mr: %d, target=%d, l,m,r = %d %d %d\n", sum_mr, target, l,m,r);
+                    ans.add(Arrays.asList(nums[l],nums[m],nums[r]));
+
+                }else if (sum_mr < target) {
+                    m++;
+                }else {
+                    r--;
+                }
             }
         }
         return ans;
