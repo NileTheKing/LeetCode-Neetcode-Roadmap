0b3fe6bfaf946eb20fed09eaa3f3b15bf57263ef
양나일, Nile Yang <nileyangdev@gmail.com>
2026-03-13 17:52:30 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3832-find-users-with-persistent-behavior-patterns/3832-find-users-with-persistent-behavior-patterns.sql
+++ 3832-find-users-with-persistent-behavior-patterns/3832-find-users-with-persistent-behavior-patterns.sql
@@ -1,26 +1,26 @@
-with stable_days as(
-    select user_id, action_date, max(action) as action
+with stable_days as (
+    select user_id, action_date, action
     from activity
     group by user_id, action_date
-    having count(action) = 1
+    having count(*) = 1
 ),
 streak as (
     select user_id, action, count(*) as streak_length,
     min(action_date) as start_date,
     max(action_date) as end_date
     from (
-        select *,
-            date_sub(action_date, interval row_number() over (partition by user_id, action
-             order by action_date) day) as grp
+        select *, 
+        date_sub(action_date, interval row_number() over (partition by user_id, action order by action_date) day) as grp
         from stable_days
     ) as t
-    group by user_id, action, grp
+    group by user_id, action
 )
+
 select user_id, action, streak_length, start_date, end_date
 from (
     select *, rank() over (partition by user_id order by streak_length desc) as rnk
     from streak
     where streak_length >= 5
-) t
+) as t
 where rnk = 1
 order by streak_length desc, user_id asc;
