caecb7c297a79ef7f504631c46c32a567076bf80
HeadedNowhere <nileyangdev@gmail.com>
2026-06-19 16:52:21 +0900

    Time: 265 ms (15.01%), Space: 59.4 MB (24.64%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,30 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
+        //3 * 10^3.  o(n^3) 불가능 o(n^2)가능. 
         List<List<Integer>> ans = new ArrayList<>();
         for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i -1]) continue;
             int l = i;
+            if (l > 0 && nums[l] == nums[l - 1]) continue;
+            System.out.printf("=== %d ===\n", l);
             int m = i + 1;
             int r = nums.length - 1;
-            int target = -nums[l];
             while (m < r) {
-                if (m > l + 1 && nums[m] == nums[m - 1]) {
+                if (m > i + 1 && nums[m] == nums[m - 1]) {
                     m++;
                     continue;
                 }
-                int sum_mr = nums[m] + nums[r];
-                if (sum_mr == target) {
-                    // System.out.printf("sum_mr: %d, target=%d, l,m,r = %d %d %d\n", sum_mr, target, l,m,r);
-                    ans.add(Arrays.asList(nums[l],nums[m],nums[r]));
-
-                }else if (sum_mr < target) {
+                // System.out.printf("     ===%d %d %d===\n", l, m, r);
+                int target = -nums[l]; //nums[m] + nums[r]했을떄 -nums[l]나와야한다
+                int sum = nums[m] + nums[r];// [m] + [r]
+                if (sum > target) {
+                    r--;
+                }else if (sum < target) {
                     m++;
                 }else {
-                    r--;
+                    // System.out.printf("%d %d %d\n", l, m, r);
+                    ans.add(Arrays.asList(nums[l], nums[m], nums[r]));
+                    m++;
                 }
             }
-        }
+        } 
         return ans;
+
     }
 }
