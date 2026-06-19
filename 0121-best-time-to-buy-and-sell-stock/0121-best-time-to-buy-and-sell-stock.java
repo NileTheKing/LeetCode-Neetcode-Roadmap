7d7238bff093c25947bcb3f6c78003d9bf04ffec
HeadedNowhere <nileyangdev@gmail.com>
2026-06-19 16:24:16 +0900

    Time: 1 ms (99.94%), Space: 94.5 MB (50.3%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,29 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        //싸게사서 비싸게 팔면 좋다.
-        //각 날짜에서 제일 싸게살수 있을때가 언제잉ㄴ지 (왼쪽)
-        //랑 오른쪽바라볼떄 제일 비싼가격이 뭔지.
-        int len = prices.length;
-        int[] cheap = new int[len];
-        int[] expensive = new int[len];
         int cheapiest = Integer.MAX_VALUE;
-        int mostExpensive = Integer.MIN_VALUE;
+        int maxProfit = 0;
 
-        for (int i = 0; i < len; i++) {
-            cheap[i] = Math.min(cheapiest, prices[i]);
-            cheapiest = Math.min(cheapiest, cheap[i]);
-            // System.out.printf("%d, %d\n", i, cheap[i]);
+        for (int p : prices) {
+            cheapiest = Math.min(cheapiest, p);
+            maxProfit = Math.max(maxProfit, p - cheapiest);
         }
-        for (int i = len - 1; i >= 0; i--) {
-            expensive[i] = Math.max(mostExpensive, prices[i]);
-            mostExpensive = Math.max(mostExpensive, expensive[i]);
-            // System.out.printf("%d, %d\n", i, expensive[i]);
-        }
-        int ans = 0;
-        for (int i = 0; i < len; i++) {
-            // System.out.printf("%d %d\n", cheap[i], expensive[i]);
-            ans = Math.max(ans, expensive[i] - cheap[i]);
-        }
-        return ans;
+        return maxProfit;
     }
 }
