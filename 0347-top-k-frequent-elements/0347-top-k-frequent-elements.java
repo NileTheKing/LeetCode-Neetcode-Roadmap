72a57934dfbdc4eec56d131c256574501f7a36d8
HeadedNowhere <nileyangdev@gmail.com>
2026-06-19 15:29:35 +0900

    Time: 16 ms (40.67%), Space: 47.6 MB (47.98%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        //빈도수 저장
+        Map<Integer, Integer> freq = new HashMap<>(); //freq: <값, 빈도>
+        for (int n : nums) freq.put(n, freq.getOrDefault(n, 0) + 1);
+        //이제 큰거부터 꺼내기. 최대힙을 쓰면 좋을듯
+        //int[] : {값, 빈도수}
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((o1, o2) -> o2[1] - o1[1]);
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            maxHeap.offer(new int[] {entry.getKey(), entry.getValue()});
         }
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[1] - a[1]); //빈도수 기준 내림차순
-        for (int key : map.keySet()) {
-            pq.offer(new int[]{key, map.get(key)});
-        }
-
         List<Integer> ans = new ArrayList<>();
-        while (k > 0 && !pq.isEmpty()) {
-            int[] polled = pq.poll();
+        for (int i = 0; i < k; i++) {
+            if (maxHeap.isEmpty()) return new int[0]; //error
+            int[] polled = maxHeap.poll();
             ans.add(polled[0]);
-            k--;
         }
-
         return ans.stream().mapToInt(i -> i).toArray();
     }
 }
