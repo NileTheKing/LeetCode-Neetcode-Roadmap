f8a5503ab3ccfd4a01a3013e8d848068994ffe98
HeadedNowhere <nileyangdev@gmail.com>
2026-06-19 15:10:01 +0900

    Time: 4 ms (49.61%), Space: 46.4 MB (98.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,22 +1,10 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[][] pair = new int[nums.length][2];//2는 값과 인덱스
+        Map<Integer, Integer> map = new HashMap<>(); //값, 인덱스
+        for (int i = 0; i < nums.length; i++) map.put(nums[i], i);
         for (int i = 0; i < nums.length; i++) {
-            pair[i][0] = nums[i];
-            pair[i][1] = i;
-        }
-        Arrays.sort(pair, (o1, o2) -> o1[0] - o2[0]);//값정렬
-        int l = 0;
-        int r = nums.length - 1;
-        while (l < r) {
-            int sum = pair[l][0] + pair[r][0];
-            if (sum > target) {
-                r--;
-            }else if (sum < target) {
-                l++;
-            }else {
-                return new int[] {pair[l][1], pair[r][1]};
-            }
+            int a = target - nums[i]; //더해야하는값 nums[i]제어ㅚ
+            if (map.containsKey(a) && map.get(a) != i) return new int[] {i, map.get(a)};
         }
         return new int[0];
     }
