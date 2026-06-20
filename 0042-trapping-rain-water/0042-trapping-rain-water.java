854e3622ee6df2c5472d710953aea0e089de7284
HeadedNowhere <nileyangdev@gmail.com>
2026-06-20 20:22:30 +0900

    Time: 1 ms (59.22%), Space: 47.8 MB (55.91%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,26 +1,23 @@
 class Solution {
     public int trap(int[] height) {
-        
         int len = height.length;
-        int[] maxl = new int[len];
-        int[] maxr = new int[len];
-        
-        maxl[0] = Integer.MIN_VALUE;
-        for (int i = 1; i < len; i++) {
-            maxl[i] = Math.max(maxl[i - 1], height[i - 1]);
-        }
-        maxr[len - 1] = Integer.MIN_VALUE;
-        for (int i = len - 1 - 1; i > 0; i--) {
-            maxr[i] = Math.max(maxr[i + 1], height[i + 1]);
-        }
-        
+        int l = 0;
+        int r = len - 1;
+        int leftMax = height[l];
+        int rightMax = height[r];
         int sum = 0;
-        for (int i = 1; i < len - 1; i++) {
-            int min_from_maxlr = Math.min(maxl[i], maxr[i]);
-            if (height[i] >= min_from_maxlr) continue; //담을 물이 없음
-            int diff = min_from_maxlr - height[i];
-            sum += diff;
+        while (l <= r) {
+            if (leftMax < rightMax) {
+                sum += Math.max(0, leftMax - height[l]);
+                leftMax = Math.max(leftMax, height[l]);
+                l++;
+            }else {
+                sum += Math.max(0, rightMax - height[r]);
+                rightMax = Math.max(rightMax, height[r]);
+                r--;
+            }
         }
         return sum;
+        
     }
 }
